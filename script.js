55a49899dedbe62ae1e2bb41fbddc45623ed433c
andrew schwirzinski <aschwirzinski@gmail.com>
2022-07-04 16:23:15 -0400

    add .again btn event handler to restore defaults

--- script.js
+++ script.js
@@ -11,8 +11,6 @@
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 
-document.querySelector(".number").textContent = secretNumber;
-
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
   console.log(guess, typeof guess);
@@ -22,6 +20,7 @@ document.querySelector(".check").addEventListener("click", function () {
     //player wins
   } else if (guess == secretNumber) {
     document.querySelector(".message").textContent = "Correct Number";
+    document.querySelector(".number").textContent = secretNumber;
 
     document.querySelector("body").style.backgroundColor = "#60b347";
     document.querySelector(".number").style.width = "30rem";
@@ -52,4 +51,16 @@ document.querySelector(".check").addEventListener("click", function () {
     // score--;
     // document.querySelector(".score").textContent = score;
   }
+});
+
+//resets everything to default values
+document.querySelector(".again").addEventListener("click", function () {
+  const secretNumber = Math.trunc(Math.random() * 20) + 1;
+  let score = 20;
+  document.querySelector(".score").textContent = score;
+  document.querySelector(".number").textContent = "?";
+  document.querySelector(".message").textContent = "Start guessing...";
+  document.querySelector("body").style.backgroundColor = "#222";
+  document.querySelector(".guess").value = " ";
+  document.querySelector(".number").style.width = "15rem";
 });
