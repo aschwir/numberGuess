1b4cb40f658e0920cef335a1d5204ee11a1a4c8a
andrew schwirzinski <aschwirzinski@gmail.com>
2022-07-04 00:03:26 -0400

    add score logic

--- script.js
+++ script.js
@@ -8,8 +8,10 @@
 // document.querySelector(".guess").value = 23;
 // console.log(document.querySelector(".guess").value);
 
-const number = Math.trunc(Math.random() * 20) + 1;
-document.querySelector(".number").textContent = number;
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+
+document.querySelector(".number").textContent = secretNumber;
 
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
@@ -17,5 +19,15 @@ document.querySelector(".check").addEventListener("click", function () {
 
   if (!guess) {
     document.querySelector(".message").textContent = "No number";
+  } else if (guess == secretNumber) {
+    document.querySelector(".message").textContent = "Correct Number";
+  } else if (guess > secretNumber) {
+    document.querySelector(".message").textContent = "Too High!";
+    score--;
+    document.querySelector(".score").textContent = score;
+  } else {
+    document.querySelector(".message").textContent = "Too Low";
+    score--;
+    document.querySelector(".score").textContent = score;
   }
 });
