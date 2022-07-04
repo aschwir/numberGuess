0321bd04c84a422b01c3c8828be46012945848d2
andrew schwirzinski <aschwirzinski@gmail.com>
2022-07-04 15:48:08 -0400

    add class event handlers

--- script.js
+++ script.js
@@ -16,25 +16,35 @@ document.querySelector(".number").textContent = secretNumber;
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
   console.log(guess, typeof guess);
-
+  //When no number was input
   if (!guess) {
     document.querySelector(".message").textContent = "No number";
+    //player wins
   } else if (guess == secretNumber) {
     document.querySelector(".message").textContent = "Correct Number";
+
+    document.querySelector("body").style.backgroundColor = "#60b347";
+    document.querySelector(".number").style.width = "30rem";
+    //if guess is greater than the secret number
   } else if (guess > secretNumber) {
+    //before score reached 0
     if (score > 1) {
       document.querySelector(".message").textContent = "Too High!";
       score--;
       document.querySelector(".score").textContent = score;
+      //score reaches 0 and game is lost
     } else {
       document.querySelector(".message").textContent = "You lost the game!!";
       document.querySelector(".score").textContent = 0;
     }
+    //if guess is lower than the secret number
   } else {
+    //score is above 0
     if (score > 1) {
       document.querySelector(".message").textContent = "Too Low!";
       score--;
       document.querySelector(".score").textContent = score;
+      //score reaches 0 and game is lost
     } else {
       document.querySelector(".message").textContent = "You lost the game!!";
       document.querySelector(".score").textContent = 0;
